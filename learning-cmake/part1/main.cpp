344da82a27f67b5cbb7740b861ac89080c51acb4
Ryan Robinson <ryantherileyman@outlook.com>
2021-03-17 18:52:11 -0500

    TCC-32 Add step to copy resource files

--- learning-cmake/part1/main.cpp
+++ learning-cmake/part1/main.cpp
@@ -1,7 +1,19 @@
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-	std::cout << "Hello, CMake!";
+	
+	std::ifstream greetingFile("resources/greeting.txt");
+	if (greetingFile.is_open()) {
+		std::string greetingText;
+		getline(greetingFile, greetingText);
+		std::cout << greetingText;
+	}
+	else {
+		std::cout << "Error: " << greetingFile.rdstate();
+	}
+
 	return 0;
 }
