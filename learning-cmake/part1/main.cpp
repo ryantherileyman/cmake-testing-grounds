c17616bf1a8600bcd9da13c2d869d5f22f1cc277
Ryan Robinson <ryantherileyman@outlook.com>
2021-03-18 21:04:17 -0500

    TCC-32 Add step to configure a file with build info

--- learning-cmake/part1/main.cpp
+++ learning-cmake/part1/main.cpp
@@ -3,16 +3,42 @@
 #include <fstream>
 #include <string>
 
-int main() {
+int main(int argc, char* argv[]) {
 	
-	std::ifstream greetingFile("resources/greeting.txt");
-	if (greetingFile.is_open()) {
-		std::string greetingText;
-		getline(greetingFile, greetingText);
-		std::cout << greetingText;
+	bool printGreetingFlag = true;
+	if (argc > 1) {
+		if (strcmp(argv[1], "-v") == 0) {
+			std::string versionString;
+			std::string buildString;
+
+			std::ifstream aboutFile("resources/about.txt");
+			std::string aboutLine;
+			while (std::getline(aboutFile, aboutLine)) {
+				if (aboutLine.find("version=") == 0) {
+					versionString = aboutLine.substr(8);
+				}
+				else if (aboutLine.find("build=") == 0) {
+					buildString = aboutLine.substr(6);
+				}
+			}
+
+			std::cout << "Version: " << versionString << "\n";
+			std::cout << "Build Date: " << buildString << "\n";
+
+			printGreetingFlag = false;
+		}
 	}
-	else {
-		std::cout << "Error: " << greetingFile.rdstate();
+
+	if (printGreetingFlag) {
+		std::ifstream greetingFile("resources/greeting.txt");
+		if (greetingFile.is_open()) {
+			std::string greetingText;
+			getline(greetingFile, greetingText);
+			std::cout << greetingText;
+		}
+		else {
+			std::cout << "Error: " << greetingFile.rdstate();
+		}
 	}
 
 	return 0;
